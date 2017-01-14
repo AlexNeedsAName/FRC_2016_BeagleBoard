607f6ebbb4513e1c930b382bb4bc61a5a068e91a
AlekMabry <alekmabry24@gmail.com>
2017-01-14 07:01:42 +0000

    Boiler Line now finds angle of line

--- BoilerLine.py
+++ BoilerLine.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
-
+import math
+import sys
+import time
 
 video_capture = cv2.VideoCapture(-1)
 video_capture.set(3, 160)
@@ -18,8 +20,8 @@ while(True):
     hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
 
     # define range of white color in HSV
-    lower_white = np.array([20, 100, 100])
-    upper_white = np.array([80, 255, 255])
+    lower_white = np.array([0, 40, 0])
+    upper_white = np.array([10, 255, 255])
 
     # Threshold the HSV image to get only white colors
     thresh = cv2.inRange(hsv, lower_white, upper_white)
@@ -33,13 +35,17 @@ while(True):
     # Find the biggest contour (if detected)
     if len(contours) > 0:
         c = max(contours, key=cv2.contourArea)
-        M = cv2.moments(c)
+        #M = cv2.moments(c)
+	
+	#if M['m00'] is 0:
+        #    cx = int(M['m10'])
+        #    cy = int(M['m01'])
+        #else:
+	#    cx = int(M['m10']/M['m00'])
+        #    cy = int(M['m01']/M['m00'])
 
-        cx = int(M['m10']/M['m00'])
-        cy = int(M['m01']/M['m00'])
-
-        cv2.line(frame,(cx,0),(cx,720),(255,0,0),1)
-        cv2.line(frame,(0,cy),(1280,cy),(255,0,0),1)
+        #cv2.line(frame,(cx,0),(cx,720),(255,0,0),1)
+        #cv2.line(frame,(0,cy),(1280,cy),(255,0,0),1)
 
         # then apply fitline() function
         [vx, vy, x, y] = cv2.fitLine(c, cv2.cv.CV_DIST_L2, 0, 0.01, 0.01)
@@ -51,12 +57,35 @@ while(True):
         # Finally draw the line
         cv2.line(frame, (thresh.shape[1] - 1, righty), (0, lefty), 255, 2)
 
+        xDifference = (thresh.shape[1]-1) - 0
+        yDifference = righty-lefty
+
+        pythagOne = (xDifference*xDifference) + (yDifference*yDifference)
+        pythagTwo = math.sqrt(pythagOne)
+
+        #Normalize the vector
+        xDifference = (1/pythagTwo)*xDifference
+        yDifference = (1/pythagTwo)*yDifference
+
+        lineRadians = math.atan(yDifference/xDifference)
+
+        lineDegrees = math.degrees(lineRadians)
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(frame, str(lineDegrees), (10, 120), font, 1, (255, 0, 0), 2)
+
         cv2.drawContours(frame, contours, -1, (255,0,0), 1)
 
+        sys.stdout.write(str(lineDegrees)+"\n")
+
     else:
-        print "I don't see the line"
+        sys.stdout.write("0\n")
+
+    sys.stdout.flush()
 
     #Display the resulting frame
-    cv2.imshow('frame',frame)
+    #cv2.imshow('frame',frame)
+    time.sleep(1)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+

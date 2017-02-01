4994bf29131dd3cd546cbbcbf3400ca4ab866dd1
AlekMabry <AlekMabry24@gmail.com>
2017-02-01 10:44:41 +0530

    Re-added tester so that we can view the openCV code for debugging

---
+++ VisionTester.py
@@ -0,0 +1,44 @@
+import numpy as np
+import cv2
+import math
+import sys
+import time
+import socket
+
+import BoilerLine
+import BoilerStack
+import LineFollower
+
+video_capture = cv2.VideoCapture(-1)
+video_capture.set(3, 320)
+video_capture.set(4, 240)
+
+#Change these two values to run different conencted programs
+showVideo = 1
+data = 3
+
+while(True):
+    if data == 0:
+        ret, frame = video_capture.read()
+        sendData = BoilerLine.findBoilerLine(ret, frame)
+    elif data == 1 in data:
+        ret, frame = video_capture.read()
+        sendData = BoilerStack.findBoilerStack(ret, frame)
+    elif data == 2 in data:
+        ret, frame = video_capture.read()
+        sendData = LineFollower.lineOffset(ret, frame)
+    elif data == 3:
+        ret, frame = video_capture.read()
+        x,y = SpringDetect.findSpring(ret, frame)
+        params = springParams
+        sendData = PixelsToDegrees.screenPixelsToDegrees(x,y,params)
+    else:
+        sendData = 'Bad request: ' + data
+
+    print sendData
+
+    #Quit Key
+    if showVideo == 1:
+        cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break

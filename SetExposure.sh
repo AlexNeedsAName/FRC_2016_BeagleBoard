8fee8fe3d343498320b6c09b9f8df52b2cc7f48b
Jack LeFevre <jackflef@gmail.com>
2017-02-12 14:19:51 -0500

    updated set exposure

--- SetExposure.sh
+++ SetExposure.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-v4l2-ctl -d /dev/video0 -c exposure_auto=1 -c exposure_absolute=$1
+#Find the video in /dev, then set exposure to zero
+
+video=$(sudo find /dev -name "video*")
+
+v4l2-ctl -d "$video" -c exposure_auto=1 -c exposure_absolute=$1

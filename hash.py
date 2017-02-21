7ae49e390fcb7f5f85bd29272a598fbec00a8562
AlexNeedsAName <Alexander@theAlexanderMartin.com>
2017-02-21 17:28:54 -0500

    Fixed Hash I gave up on getting python to do it right, so I have the python execute java code :P

--- hash.py
+++ hash.py
@@ -1,22 +1,6 @@
 #!/usr/bin/env python
-import sys
-def rshift(val, n):
-	if(val >= 0):
-		return val>>n
-	else:
-		return (val+0x100000000)>>n
+import subprocess
 
 def oneAtATime(msg):
-	key = bytearray()
-	key.extend(map(ord, msg))
-
-	hash = 0
-	for byte in key:
-		hash += (byte)
-		hash += (hash << 10)
-		hash ^= (rshift(hash, 6))
-		
-	hash += (hash << 3)
-	hash ^= (rshift(hash, 11))
-	hash += (hash << 15)
-	return hash
+	#I gave up on getting python to do the hash :P
+	return (subprocess.check_output(['java', 'Hash', msg]))
